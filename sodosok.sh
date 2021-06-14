8c80a4648e240d527ae99752e948a113d95175fb
di2nk <79467462+di2nk@users.noreply.github.com>
2021-06-14 21:46:17 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -90,11 +90,11 @@ ip6tables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEP
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O addss "https://netzvps.herokuapp.com/vpn/addss.sh"
-wget -O delss "https://netzvps.herokuapp.com/vpn/delss.sh"
-wget -O cekss "https://netzvps.herokuapp.com/vpn/cekss.sh"
-wget -O xp-ss "https://netzvps.herokuapp.com/vpn/xp-ss.sh"
-wget -O renewss "https://netzvps.herokuapp.com/vpn/renewss.sh"
+wget -O addss "https://raw.githubusercontent.com/di2nk/all/main/addss.sh"
+wget -O delss "https://raw.githubusercontent.com/di2nk/all/main/delss.sh"
+wget -O cekss "https://raw.githubusercontent.com/di2nk/all/main/cekss.sh"
+wget -O xp-ss "https://raw.githubusercontent.com/di2nk/all/main/xp-ss.sh"
+wget -O renewss "https://raw.githubusercontent.com/di2nk/all/main/renewss.sh"
 chmod +x addss
 chmod +x delss
 chmod +x cekss
